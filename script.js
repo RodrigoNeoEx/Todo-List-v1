4a7284ed287251ab703fd7b1c05f63315ab140ef
RodrigoNeoEx <draja.acc@gmail.com>
2021-05-20 19:37:08 -0300

    created input, btnSave and createTask

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ function createFirstSection() {
   append.appendChild(firstLineSection);
 }
 
+function createBtnTasks() {
+  const createButton = document.createElement('button');
+  createButton.id = 'criar-tarefa';
+  createButton.innerText = 'Create Task';
+  createButton.classList = 'styleHeadButtons';
+  firstLineSection.appendChild(createButton);
+}
+
+function createInputAssigment() {
+  const createInput = document.createElement('input');
+  createInput.id = 'texto-tarefa';
+  createInput.setAttribute('type', 'text');
+  createInput.name = 'inputAssigment';
+  createInput.classList = 'styleHeadButtons';
+  firstLineSection.appendChild(createInput);
+}
+
+function createBtnSaveAllAssigments() {
+  const saveAllAssigment = document.createElement('button');
+  saveAllAssigment.id = 'salvar-tarefas';
+  saveAllAssigment.innerText = 'Save Tasks';
+  saveAllAssigment.classList = 'styleHeadButtons';
+  firstLineSection.appendChild(saveAllAssigment);
+}
+
